d560623b1bd7e999a59bfdb4119006a68eb93ace
Saulo Assis <saulosilvaassis@gmail.com>
2021-06-28 16:09:48 -0300

    v3

--- avaliacao1.js
+++ avaliacao1.js
@@ -1,50 +1,85 @@
-// Saulo Assis
-
-/*  
-- O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-- Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
-- Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
-- Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
-- No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!".
-*/
+// Saulo Assis - Turma 4
 
 
 function menu(prato, tempo) {
 
     let tempoTotal = tempo
+    let temponecessario = 0
 
-    let pipoca = 10
-    let macarao = 8
-    let carne = 15
-    let feijao = 12
-    let brigadeiro = 8
+    let tempoPipoca = 10
+    let tempoMacarrao = 8
+    let tempoCarne = 15
+    let tempoFeijao = 12
+    let tempoBrigadeiro = 8
 
 
     if (prato == "Pipoca") {
-        tempoTotal += pipoca
+        tempoTotal += tempoPipoca
+        temponecessario = 10
     } else if (prato == "Macarrão") {
-        tempoTotal += macarrao
+        tempoTotal += tempoMacarrao
+        temponecessario = 8
     } else if (prato == "Carne") {
-        tempoTotal += carne
+        tempoTotal += tempoCarne
+        temponecessario = 15
     } else if (prato == "Feijão") {
-        tempoTotal += feijao
+        tempoTotal += tempoFeijao
+        temponecessario = 12
     } else if (prato == "Brigadeiro") {
-        tempoTotal += brigadeiro
+        tempoTotal += tempoBrigadeiro
+        temponecessario = 8
     } else {
         console.log("Escolha uma opção válida")
     }
 
 
 
-    if (tempoTotal > prato * 2) {
+    if (tempoTotal >= (temponecessario * 3)) {
+        console.log("kabum!")
+
+    } else if (tempoTotal >= (temponecessario * 2)) {
         console.log("A comida queimou!")
 
-    } else of(tempoTotal > prato * 3){
-        console.log("Kabum")
-
-    }else of(tempoTotal < prato){
+    } else if (tempoTotal < temponecessario) {
         console.log("Tempo insuficiente")
 
-    }else
-    console.log("Prato pronto, bom apetite!!")
+    } else {
+        console.log("Prato pronto, bom apetite!!")
+    }
 }
+
+
+
+
+menu("Pipoca", 0)
+menu("Pipoca", 10)
+menu("Pipoca", 22)
+menu("Pipoca", -5)
+
+console.log("\n")
+
+menu("Macarrão", 0)
+menu("Macarrão", 10)
+menu("Macarrão", 22)
+menu("Macarrão", -5)
+
+console.log("\n")
+
+menu("Carne", 0)
+menu("Carne", 10)
+menu("Carne", 22)
+menu("Carne", -5)
+
+console.log("\n")
+
+menu("Feijão", 0)
+menu("Feijão", 10)
+menu("Feijão", 22)
+menu("Feijão", -5)
+
+console.log("\n")
+
+menu("Brigadeiro", 0)
+menu("Brigadeiro", 10)
+menu("Brigadeiro", 22)
+menu("Brigadeiro", -5)
