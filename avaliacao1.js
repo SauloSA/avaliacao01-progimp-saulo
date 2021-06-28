29944f8511d1c77e9926e5dbac2ff6ba25679f67
Saulo Assis <saulosilvaassis@gmail.com>
2021-06-28 15:57:43 -0300

    v2

--- avaliacao1.js
+++ avaliacao1.js
@@ -1,7 +1,50 @@
 // Saulo Assis
 
+/*  
 - O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
 - Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
 - Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
 - Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
 - No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!".
+*/
+
+
+function menu(prato, tempo) {
+
+    let tempoTotal = tempo
+
+    let pipoca = 10
+    let macarao = 8
+    let carne = 15
+    let feijao = 12
+    let brigadeiro = 8
+
+
+    if (prato == "Pipoca") {
+        tempoTotal += pipoca
+    } else if (prato == "Macarrão") {
+        tempoTotal += macarrao
+    } else if (prato == "Carne") {
+        tempoTotal += carne
+    } else if (prato == "Feijão") {
+        tempoTotal += feijao
+    } else if (prato == "Brigadeiro") {
+        tempoTotal += brigadeiro
+    } else {
+        console.log("Escolha uma opção válida")
+    }
+
+
+
+    if (tempoTotal > prato * 2) {
+        console.log("A comida queimou!")
+
+    } else of(tempoTotal > prato * 3){
+        console.log("Kabum")
+
+    }else of(tempoTotal < prato){
+        console.log("Tempo insuficiente")
+
+    }else
+    console.log("Prato pronto, bom apetite!!")
+}
